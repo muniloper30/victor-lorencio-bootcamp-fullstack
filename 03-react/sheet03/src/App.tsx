22e3c83a03223d5752aa2de5cff6abcee3e5da0e
Victor Lorencio <victorlorencio@gmail.com>
2026-04-14 23:53:02 +0200

    feat(react): complete exercise12 from sheet03 - fix react component using spread operator

--- 03-react/sheet03/src/App.tsx
+++ 03-react/sheet03/src/App.tsx
@@ -2,17 +2,19 @@
 // import { Exercise03 } from './exercise03/Exercise03'
 // import { Exercise05 } from "./exercise05/Exercise"
 // import { Exercise08 } from "./exercise08/Exercise08"
-import { Exercise11 } from "./exercise11/Exercise11"
+// import { Exercise11 } from "./exercise11/Exercise11"
+import { Exercise12 } from "./exercise12/Exercise12"
 
 function App() {
- 
+
 
   return (
     <>
      {/* <Exercise03></Exercise03>  */}
      {/* <Exercise05></Exercise05> */}
      {/* <Exercise08></Exercise08> */}
-     <Exercise11></Exercise11>
+     {/* <Exercise11></Exercise11> */}
+     <Exercise12></Exercise12>
     </>
   )
 }
---
+++ 03-react/sheet03/src/exercise12/Exercise12.tsx
@@ -0,0 +1,9 @@
+import { ContadorItem } from "./ContadorItem/ContadorItem"
+
+export const Exercise12 = () => {
+    return (
+        <div>
+            <ContadorItem></ContadorItem>
+        </div>
+    )
+}
