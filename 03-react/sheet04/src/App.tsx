db6985ddbc0d38c7dd8b9733c6eb1d13e44c7e50
Victor Lorencio <victorlorencio@gmail.com>
2026-04-16 16:55:53 +0200

    feat(react): fix react component from sheet04 exercise03

---
+++ 03-react/sheet04/src/App.tsx
@@ -0,0 +1,15 @@
+import { Exercise09 } from './Exercise03/Exercise03'
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+  <Exercise09></Exercise09>
+    </>
+  )
+}
+
+export default App
---
+++ 03-react/sheet04/src/Exercise03/Exercise03.tsx
@@ -0,0 +1,9 @@
+import { SampleAccess } from "./SampleAccess/SampleAccess"
+
+export const Exercise09 = () => {
+    return (
+        <div className="flex flex-col items-center justify-center min-h-screen">
+            <SampleAccess></SampleAccess>
+        </div>
+    )
+}
