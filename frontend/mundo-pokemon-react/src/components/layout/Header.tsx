d4212735702394d6bff16e78b6796c414e65c002
Victor Lorencio <victorlorencio@gmail.com>
2026-04-22 14:40:25 +0200

    feat(react): add bar pokemons filter

--- frontend/mundo-pokemon-react/src/components/layout/Header.tsx
+++ frontend/mundo-pokemon-react/src/components/layout/Header.tsx
@@ -1,10 +1,13 @@
+
 import { PokemonSearch } from "../pokemon/PokemonSearch"
+import type { HeaderProps } from "../../services/pokemonApi";
 
-export const Header = () => {
+  
+
+export const Header = ({ onSearch }: HeaderProps) => {
     return (
-        <div className="flex flex-col items-center justify-center mt-4">
-            <PokemonSearch></PokemonSearch>
+        <div className='flex flex-col items-center justify-center mt-4'>
+            <PokemonSearch onSearch={onSearch} />
         </div>
-
     )
 }
--- frontend/mundo-pokemon-react/src/components/pokemon/PokemonSearch.tsx
+++ frontend/mundo-pokemon-react/src/components/pokemon/PokemonSearch.tsx
@@ -1,12 +1,17 @@
-export const PokemonSearch = () => {
+
+import type { HeaderProps } from "../../services/pokemonApi"
+
+    export const PokemonSearch = ({onSearch} : HeaderProps) => {
 
 
 
-    return (
-        <>
-            <label htmlFor="search"></label>
-            <input className="w-full p-3 m-0 text-base text-center outline-none border border-white bg-gray-200 placeholder:text-slate-400/40 shadow-md" type="search" placeholder="Filtra pokemons por nombre..." />
-        </>
+        return (
+            <>
+                <label htmlFor="search"></label>
+                <input 
+                onChange={(e) => onSearch(e.target.value)}
+                className="w-full p-3 m-0 text-base text-center outline-none border border-white bg-gray-200 placeholder:text-slate-400/40 shadow-md" type="search" placeholder="Filtra pokemons por nombre..." />
+            </>
 
-    )
-}
+        )
+    }
